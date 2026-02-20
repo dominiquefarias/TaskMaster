82926706f1b27a55cf3f2f0d713ed9ee62cb66d6
Dominique Farias <dominiquefarias21@gmail.com>
2026-02-20 09:23:52 +0100

    .

--- Inicio/Inicio de sesion/conexion.php
+++ Inicio/Inicio de sesion/conexion.php
@@ -1,18 +1,13 @@
 <?php
-// Conecto php con la base de datos
 $servername = "localhost";
 $username = "Dominique";
 $password = "Dominique123$";
 $dbname = "task_master";
 
-// Crear conexión 
 $conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verificar conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-// Establecer charset a utf8 para evitar problemas con caracteres especiales
 $conn->set_charset("utf8");
 ?>
